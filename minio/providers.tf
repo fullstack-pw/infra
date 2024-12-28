3d851d483b01ea5d6257f650f8c908d5624d70be
pedropilla <pedropilla@gmail.com>
2024-12-28 07:01:31 +0000

    minio

---
+++ minio/providers.tf
@@ -0,0 +1,11 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+  config_context = "kubernetes-admin@kubernetes"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+    config_context = "kubernetes-admin@kubernetes"
+  }
+}
