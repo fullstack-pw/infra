128d33d7f0492c618cff95961b6c9d926aeb5e87
pedropilla <pedropilla@gmail.com>
2024-12-28 07:14:35 +0000

    mv proxmox tfstate

--- proxmox/variables.tf
+++ proxmox/variables.tf
@@ -1,3 +1,25 @@
+
+
 variable "vm_configs" {
   default = []
+}
+
+variable "proxmox_password" {}
+
+# Define the network configuration
+variable "network_config" {
+  default = {
+    dns = "192.168.1.3"
+    k01 = "192.168.1.101"
+    k02 = "192.168.1.102"
+    k03 = "192.168.1.103"
+    k8s = "192.168.1.4"
+  }
+}
+
+locals {
+  vm_configs = {
+    for file, content in data.local_file.yaml_files :
+    file => yamldecode(content.content)
+  }
 }
