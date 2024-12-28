1653463b42b7df3a126b2d9b6147db32c86ccbb4
pedropilla <pedropilla@gmail.com>
2024-12-28 07:14:35 +0000

    mv proxmox tfstate

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -1,44 +1,7 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "Telmate/proxmox"
-      version = "3.0.1-rc6"
-    }
-  }
-}
-
-provider "proxmox" {
-  pm_api_url      = "https://192.168.1.2:8006/api2/json"
-  pm_user         = "root@pam"
-  pm_password     = var.proxmox_password
-  pm_tls_insecure = true
-}
-
-variable "proxmox_password" {}
-
-# Define the network configuration
-variable "network_config" {
-  default = {
-    dns = "192.168.1.3"
-    k01 = "192.168.1.101"
-    k02 = "192.168.1.102"
-    k03 = "192.168.1.103"
-    k8s = "192.168.1.4"
-  }
-}
-
 # Read YAML files
 data "local_file" "yaml_files" {
   for_each = fileset("${path.module}/vms", "*.yaml")
   filename = "${path.module}/vms/${each.key}"
-}
-
-# Decode YAML content
-locals {
-  vm_configs = {
-    for file, content in data.local_file.yaml_files :
-    file => yamldecode(content.content)
-  }
 }
 
 # Create VMs dynamically
---
+++ proxmox/providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "3.0.1-rc6"
+    }
+  }
+  backend "s3" {
+    bucket         = "terraform"
+    key            = "proxmox.tfstate"
+    endpoints = {
+      s3 = "https://s3.fullstack.pw"
+    }
+    region         = "main"
+    skip_credentials_validation = true
+    skip_requesting_account_id = true
+    skip_metadata_api_check = true
+    skip_region_validation = true
+    use_path_style = true
+  }
+}
+provider "proxmox" {
+  pm_api_url      = "https://192.168.1.2:8006/api2/json"
+  pm_user         = "root@pam"
+  pm_password     = var.proxmox_password
+  pm_tls_insecure = true
+}
